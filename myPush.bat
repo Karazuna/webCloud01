f1a7bd2f722bec0d2c586562f3393606fdf60a8a
kchy12345@gmail.com <kchy12345@gmail.com>
2018-06-20 16:52:29 +0900

    first push

---
+++ myPush.bat
@@ -0,0 +1,3 @@
+git add --all
+git commit -m "2018-06-05, VueJS - chart CSS [04], navbar - img"
+git push -u origin master
---
+++ pages/index.vue
@@ -0,0 +1,14 @@
+<template>
+  <section class="section">
+    <nav class="navabar">
+    </nav>
+    <div class="hero is-info">
+      <div class="hero-body">
+        <div class="container">
+          <h1 class="title is-1">개사랑</h1>
+          <h2 class="subtitle">개사랑 사이트입니다.</h2>
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
